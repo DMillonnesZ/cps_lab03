c7395b80b25f440bf4fcfbf1ef4bbeab7e5f12e0
Daniel Millones <dmillonesz@gmail.com>
2026-09-03 20:08:52 -0500

    agregar cambio de prueba en StudentController para validar flujo Git colaborativo

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -4,5 +4,6 @@ public class StudentController {
     // TO DO
     public void mostrar() {
         System.out.println("Controlador sprint-2");
+        System.out.println("Cambio agregado por: [tu nombre]");
     }
 }
